c7fa24813cf30980b1c412d5decf2a3efd1e6ed4
stasthehero <stasthehero@gmail.com>
2022-12-27 01:47:28 +0300

    added findCard option

--- flex/js/main.js
+++ flex/js/main.js
@@ -1,62 +1,47 @@
 const findButton = document.getElementsByName('find-button')[0];
 const cardsField = document.querySelector('.content');
+const inputForm = document.getElementById('input-form');
 const persons = [
     {name: 'Александр Бородулин', salary: '500 тыс.рублей'},
     {name: 'Даня Ценев', salary: '500 тыс.рублей'},
     {name: 'Татьяна Зобова', salary: '250 тыс.рублей'},
     {name: 'Даня Ценев', salary: '500 тыс.рублей'}
-
 ];
+
+let drawCard = (user) => {
+    cardsField.insertAdjacentHTML(
+        'afterbegin',
+        '<div class="card">\n' +
+        '                    <div class="card__img">\n' +
+        '                        <img src="https://picsum.photos/200" alt="">\n' +
+        '                    </div>\n' +
+        '                    <div class="card__info">\n' +
+        '                        <div class="card__title">' + user.name + '</div>\n' +
+        '                        <div class="card__description">' + user.salary + '</div>\n' +
+        '                    </div>\n' +
+        '                </div>'
+    )
+}
+
+let findCard = function (userName) {
+    cardsField.innerHTML = '';
+    const filteredPerson = persons.filter((person) => {
+        return person.name.toLowerCase().includes(userName.toLowerCase());
+    });
+    filteredPerson.forEach((user) => {
+        drawCard(user);
+    })
+};
+
+findButton.addEventListener("click", () => {
+    findCard(inputForm.value);
+});
 
 
 let init = function () {
     persons.forEach((user) => {
-
-        let drawCard = () => {
-            cardsField.insertAdjacentHTML(
-                'afterbegin',
-                '<div class="card">\n' +
-                '                    <div class="card__img">\n' +
-                '                        <img src="https://picsum.photos/200" alt="">\n' +
-                '                    </div>\n' +
-                '                    <div class="card__info">\n' +
-                '                        <div class="card__title">' + user.name + '</div>\n' +
-                '                        <div class="card__description">' + user.salary + '</div>\n' +
-                '                    </div>\n' +
-                '                </div>'
-            )
-        }
-        drawCard();
-
-        findButton.addEventListener("click", () => {
-            let inputForm = document.getElementById('input-form').value;
-            let deleteCard = function () {
-                if (user.name === inputForm) {
-                    cardsField.innerHTML = '';
-                    const filteredPerson = persons.filter((person) => {
-                        return person.name === inputForm;
-                    });
-                    // console.log(filteredPerson);
-                    // cardsField.insertAdjacentHTML(
-                    //     'afterbegin',
-                    //     '<div class="card">\n' +
-                    //     '                    <div class="card__img">\n' +
-                    //     '                        <img src="https://picsum.photos/200" alt="">\n' +
-                    //     '                    </div>\n' +
-                    //     '                    <div class="card__info">\n' +
-                    //     '                        <div class="card__title">' + filteredPerson[0].name + '</div>\n' +
-                    //     '                        <div class="card__description">' + filteredPerson[0].salary + '</div>\n' +
-                    //     '                    </div>\n' +
-                    //     '                </div>'
-                    // );
-                    //
-                } else {
-                }
-
-            }
-            deleteCard();
-        })
-    })
+        drawCard(user);
+    });
 };
 
 init();
